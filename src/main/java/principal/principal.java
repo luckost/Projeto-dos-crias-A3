69f6cb5f8f28ccd83fd50f28b19e94e27400f343
legen <legen@10.150.95.248>
2024-05-27 20:00:48 -0300

    feat: adicionar classe principal
    -Adicionada classe principal.

--- src/main/java/principal/principal.java
+++ src/main/java/principal/principal.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package principal;
 
-/**
- *
- * @author legen
- */
-public class principal {
+package principal;
     
-}
+import visao.frmMenuPrincipal;
+    
+    public class principal {
+        public static void main(String[] args) {
+    // Instancia a interface gráfica
+         frmMenuPrincipal objetotela = new frmMenuPrincipal();
+    // Torna a janela visível
+          objetotela.setVisible(true);
+        }
+    }
